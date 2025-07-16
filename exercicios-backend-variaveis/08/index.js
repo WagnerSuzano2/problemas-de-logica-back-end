b6e92aa917a3c09f16c9c5d0611768f5715ef591
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-16 12:04:36 -0300

    resolvido problema 8: Calculo do perímetro e área de um círculo

---
+++ exercicios-backend-variaveis/08/index.js
@@ -0,0 +1,13 @@
+let raioDoCirculo = 1;
+
+const areaDoCirculo = Math.PI * Math.pow(raioDoCirculo, 2);
+console.log(
+  `A área do círculo com raio ${raioDoCirculo} é: ${areaDoCirculo.toFixed(2)}`
+);
+
+const perimetroDoCirculo = 2 * Math.PI * raioDoCirculo;
+console.log(
+  `O perímetro do círculo com raio ${raioDoCirculo} é: ${perimetroDoCirculo.toFixed(
+    2
+  )}`
+);
