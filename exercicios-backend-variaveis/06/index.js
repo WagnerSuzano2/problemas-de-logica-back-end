e7f2e0851cbafcdaae0adb7493d6dd56300bc258
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-16 11:22:42 -0300

    resolvendo o problema 6 velocidade media

---
+++ exercicios-backend-variaveis/06/index.js
@@ -0,0 +1,6 @@
+let distancia = 500; // distacia em metros
+let tempo = 10; // tempo em segundos
+
+const velocidade = (distancia / tempo) * 3.6; // velocidade em km/h
+
+console.log(`A velocidade é de ${velocidade.toFixed(2)} km/h`); // exibe a velocidade
