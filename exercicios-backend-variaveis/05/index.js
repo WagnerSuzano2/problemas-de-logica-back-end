bd5be32a1939df3437478006fb2c87bcce5adbe7
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-15 23:31:08 -0300

    resolvido o problema 5: Calcular a distância entre dois pontos

--- exercicios-backend-variaveis/05/index.js
+++ exercicios-backend-variaveis/05/index.js
@@ -1,1 +1,12 @@
+const x1 = 1;
+const y1 = 1;
+const x2 = 1;
+const y2 = 4;
 
+const distanciaCalculada = Math.sqrt(
+  Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)
+);
+
+console.log(
+  `A distância entre os pontos (${x1}, ${y1}) e (${x2}, ${y2}) é: ${distanciaCalculada}`
+);
