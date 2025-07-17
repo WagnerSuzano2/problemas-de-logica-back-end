c48740f6657c669f3cd82d9e36b56a240cf018b8
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-16 23:41:07 -0300

    resolvido problema 11: Volume de uma esfera

---
+++ exercicios-backend-variaveis/11/index.js
@@ -0,0 +1,8 @@
+let diametro = 6;
+const raio = diametro / 2;
+
+const volumeEsferico = (4 / 3) * Math.pow(raio, 3);
+
+console.log(
+  `O volume de uma esfera de raio ${raio} é ${volumeEsferico.toFixed(0)} PI`
+);
