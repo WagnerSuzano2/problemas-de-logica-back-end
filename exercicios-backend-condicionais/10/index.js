06f81b1adaf217fcc9eb791121f951798d399cf4
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-20 19:21:23 -0300

    resolvido problema 10: Identificando caracteres

--- exercicios-backend-condicionais/10/index.js
+++ exercicios-backend-condicionais/10/index.js
@@ -1,1 +1,28 @@
-const caractere = "E";
+const caractere = "!";
+
+if (caractere == Number(caractere) || caractere == parseInt(caractere, 10)) {
+  console.log("É um número");
+} else if (
+  caractere === "A" ||
+  caractere === "E" ||
+  caractere === "I" ||
+  caractere === "O" ||
+  caractere === "U"
+) {
+  console.log("Vogal maiúscula");
+} else if (
+  caractere === "a" ||
+  caractere === "e" ||
+  caractere === "i" ||
+  caractere === "o" ||
+  caractere === "u"
+) {
+  console.log("Vogal minúscula");
+} else if (
+  (caractere >= "A" && caractere <= "Z") ||
+  (caractere >= "a" && caractere <= "z")
+) {
+  console.log("E uma consoante");
+} else {
+  console.log("Caractere desconhecido/invalido");
+}
