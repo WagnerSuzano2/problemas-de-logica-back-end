df9be6c2722f059a2807f5509d08807682b2317b
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-20 18:43:55 -0300

    resolvido problema 7: Isenção de impostos

--- exercicios-backend-condicionais/07/index.js
+++ exercicios-backend-condicionais/07/index.js
@@ -1,5 +1,11 @@
 const aposentada = false;
 const portadoraDeDoenca = false;
-const totalDeRendimentos = 3000000; //emCentavos
+const totalDeRendimentos = 3000000;
 
-//seu código aqui
+if (aposentada || portadoraDeDoenca) {
+  console.log("ISENTA");
+} else if (totalDeRendimentos <= 2855970) {
+  console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE");
+} else {
+  console.log("PEGA LEAO");
+}
