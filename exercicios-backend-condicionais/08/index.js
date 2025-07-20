9aa813b0cadfe30000ab483a57eb465e1f32fc00
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-20 18:52:05 -0300

    resolvido problema 8: Montanha Russa Muito Assustadora

--- exercicios-backend-condicionais/08/index.js
+++ exercicios-backend-condicionais/08/index.js
@@ -3,3 +3,10 @@ const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
+if (idade < 12 || idade > 65 || possuiPatologia || altura < 150) {
+  console.log("ACESSO NEGADO ");
+} else if (ehEstudante || idade < 18) {
+  console.log("ACESSO LIBERADO MEIA ENTRADA, 10 reais");
+} else {
+  console.log("ACESSO LIBERADO, 20 reais");
+}
