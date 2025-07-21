b4bdb3b31135fdc93dcb96264c091b79112e5fc2
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-21 14:46:59 -0300

    resolvido problema 12: Dia da Semana

--- exercicios-backend-condicionais/12/index.js
+++ exercicios-backend-condicionais/12/index.js
@@ -1,1 +1,26 @@
 const diaDaSemana = 3;
+switch (diaDaSemana) {
+  case 1:
+    console.log("segunda-feira");
+    break;
+  case 2:
+    console.log("terça-feira");
+    break;
+  case 3:
+    console.log("quarta-feira");
+    break;
+  case 4:
+    console.log("quinta-feira");
+    break;
+  case 5:
+    console.log("sexta-feira");
+    break;
+  case 6:
+    console.log("sábado");
+    break;
+  case 7:
+    console.log("domingo");
+    break;
+  default:
+    console.log("O dia da semana informado não é válido.");
+}
