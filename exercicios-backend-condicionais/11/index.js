d08629bc2c480e9a7b4e744f6904ef70546db070
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-20 21:53:36 -0300

    resolvido problema 11: Valor da parcela do Sucesso Compartilhado

--- exercicios-backend-condicionais/11/index.js
+++ exercicios-backend-condicionais/11/index.js
@@ -1,8 +1,24 @@
-//renda mensal do aluno, em centavos.
 const rendaMensalEmCentavos = 300000;
-
-// Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
 const mesesDecorridos = 12;
-
-// Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
 const totalJaPagoPeloAluno = 1000000;
+
+let valorParcelaEmReais = 0;
+
+if (mesesDecorridos > 60) {
+  console.log(
+    "O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois o aluno já cumpriu o tempo de contrato."
+  );
+} else if (totalJaPagoPeloAluno >= 1800000) {
+  console.log(
+    "O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois o aluno já quitou a dívida."
+  );
+} else if (rendaMensalEmCentavos < 200000) {
+  console.log(
+    "O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois a renda do estudante está abaixo do valor mínimo de R$ 2000 reais."
+  );
+} else {
+  valorParcelaEmReais = (rendaMensalEmCentavos * 0.18) / 100; // Calcula 18% da renda, e converte para reais
+  console.log(
+    `O valor da parcela desse mês é R$ ${valorParcelaEmReais.toFixed(0)} reais`
+  );
+}
