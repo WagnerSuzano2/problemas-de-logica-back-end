17872bdc5e25ff2a7efc4cfe2fc9f05f43b161e7
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-21 20:44:01 -0300

    resolvido problema 13: Compra com Desconto

--- exercicios-backend-condicionais/13/index.js
+++ exercicios-backend-condicionais/13/index.js
@@ -1,6 +1,24 @@
-//tipo de pagamento (dinheiro, credito, debito, cheque).
 const tipoDePagamento = "credito";
-
-//valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
+let valorFinal = 0;
+
+switch (tipoDePagamento) {
+  case "debito":
+  case "dinheiro":
+    valorFinal = valorDoProduto - valorDoProduto * 0.1;
+    break;
+
+  case "credito":
+    valorFinal = valorDoProduto - valorDoProduto * 0.05;
+    break;
+
+  case "cheque":
+    valorFinal = valorDoProduto - valorDoProduto * 0.03;
+    break;
+
+  default:
+    console.log("Tipo de pagamento inválido.");
+    return;
+}
+console.log(`Valor a ser pago: R$ ${(valorFinal / 100).toFixed(2)}`);
