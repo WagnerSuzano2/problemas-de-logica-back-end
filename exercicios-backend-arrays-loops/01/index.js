3df2bb7e4de79282ea81445db4081f4f07db7537
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-22 18:28:14 -0300

    resolvido problema 1: Soma total

--- exercicios-backend-arrays-loops/01/index.js
+++ exercicios-backend-arrays-loops/01/index.js
@@ -1,1 +1,26 @@
 const numeros = [2, 3, 4, 6];
+
+let somaNumeros = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+  somaNumeros += numeros[i];
+}
+
+// O loop 'for...of' itera diretamente sobre os VALORES do array.
+// Em cada iteracao, a variavel 'numero' recebera um dos elementos do array 'numeros'.
+//for (const numero of numeros) {
+//  somaNumeros += numero; // Adiciona o numero atual a somaNumeros
+//}
+
+// O metodo 'forEach' executa uma funcao (callback) para cada elemento do array.
+// A funcao de callback pode receber o elemento (aqui 'numero') e, opcionalmente, o indice.
+//numeros.forEach(function(numero) {
+//  somaNumeros += numero; // Adiciona o numero atual a somaNumeros
+//});
+
+// Com arrow function (mais comum em JavaScript moderno):
+// numeros.forEach(numero => {
+//     somaNumeros += numero;
+// });
+
+console.log(somaNumeros);
