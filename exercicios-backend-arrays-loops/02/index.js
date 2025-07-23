e57b0af58508d1a178241822dca2a3ad90959905
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-23 16:34:51 -0300

    resolvido problema 2: Contando letras

--- exercicios-backend-arrays-loops/02/index.js
+++ exercicios-backend-arrays-loops/02/index.js
@@ -1,1 +1,34 @@
 const letras = ["A", "a", "B", "C", "E", "e"];
+let letra = "6";
+let letrasEncontradas = 0;
+
+if (!((letra >= "a" && letra <= "z") || (letra >= "A" && letra <= "Z"))) {
+  console.log("A letra deve ser uma letra do alfabeto, entre A e Z, ou a e z.");
+  process.exit(1);
+}
+
+for (let i = 0; i < letras.length; i++) {
+  if (
+    letras[i] === letra.toLocaleUpperCase() ||
+    letras[i] === letra.toLowerCase()
+  ) {
+    letrasEncontradas++;
+  }
+}
+if (letrasEncontradas > 0) {
+  console.log(
+    `Foram encontradas ${letrasEncontradas} letras ` +
+      `${letra.toLocaleUpperCase()}` +
+      " ou " +
+      `${letra.toLocaleLowerCase()}` +
+      "."
+  );
+} else {
+  console.log(
+    "Nenhuma letra " +
+      `${letra.toLocaleUpperCase()}` +
+      " ou " +
+      `${letra.toLocaleLowerCase()}` +
+      " foi encontrada."
+  );
+}
