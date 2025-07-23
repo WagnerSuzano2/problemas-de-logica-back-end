c082c568c039c9a6232d4f2171e25c33e71d2fa3
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-23 20:10:06 -0300

    resolvido problema 7: Filtrando nomes com a letra A

--- exercicios-backend-arrays-loops/07/index.js
+++ exercicios-backend-arrays-loops/07/index.js
@@ -1,1 +1,11 @@
 const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+const nomesComA = [];
+
+for (let i = 0; i < nomes.length; i++) {
+  let inicial = nomes[i];
+  if (inicial[0].toLocaleLowerCase() === "a") {
+    nomesComA.push(nomes[i]);
+  }
+}
+
+console.log(nomesComA); 
