2d7b142a48560049b9807cbdb7dceade49651902
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-23 19:54:24 -0300

    resolvido problema 6: Soma dos pares

--- exercicios-backend-arrays-loops/06/index.js
+++ exercicios-backend-arrays-loops/06/index.js
@@ -1,2 +1,8 @@
 const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
-
+let somaDePares = 0;
+for (let i = 0; i < numeros.length; i++) {
+  if (numeros[i] % 2 === 0) {
+    somaDePares += numeros[i];
+  }
+}
+console.log(`A soma dos números pares é: ${somaDePares}`);
