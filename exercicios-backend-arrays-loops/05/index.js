84a9901c8a19743f7595f8cef6eecf3b14abef1c
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-23 19:51:21 -0300

    resolvido problema 5: Filtrando apenas os pares

--- exercicios-backend-arrays-loops/05/index.js
+++ exercicios-backend-arrays-loops/05/index.js
@@ -1,1 +1,9 @@
 const original = [1, 4, 12, 21, 53, 88, 112];
+const arrayDePares = [];
+
+for (const pares of original) {
+  if (pares % 2 === 0) {
+    arrayDePares.push(pares);
+  }
+}
+console.log(arrayDePares);
