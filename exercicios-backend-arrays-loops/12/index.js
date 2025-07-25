b74d809f3698927e37449499ed88db07d3466c3b
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-25 00:05:54 -0300

    resolvido problema 12: Fila do banco

--- exercicios-backend-arrays-loops/12/index.js
+++ exercicios-backend-arrays-loops/12/index.js
@@ -1,3 +1,12 @@
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
 
+const LIMITE_DENTRO = 5;
+
+while (filaDeFora.length > 0 && filaDeDentro.length < LIMITE_DENTRO) {
+  const pessoaEntrando = filaDeFora.shift();
+  filaDeDentro.push(pessoaEntrando);
+}
+
+console.log(filaDeDentro);
+console.log(filaDeFora);
