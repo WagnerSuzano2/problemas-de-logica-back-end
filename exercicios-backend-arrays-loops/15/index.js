3d3c9e32ea6cba9c38817f5c5e53ca07629c73e7
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-25 00:44:02 -0300

    resolvido problema 15: Separando pares de ímpares

--- exercicios-backend-arrays-loops/15/index.js
+++ exercicios-backend-arrays-loops/15/index.js
@@ -2,3 +2,35 @@ const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = [];
 const impares = [];
 
+
+
+for (let i = 0; i < original.length; i++) {
+  const element = original[i];
+  if (element % 2 === 0) {
+    pares.push(element);
+  } else {
+    impares.push(element);
+  }
+}
+
+
+
+for (const numeros of original) {
+  if (numeros % 2 === 0) {
+    pares.push(numeros);
+  } else {
+    impares.push(numeros);
+  }
+}
+
+
+original.forEach((element) => {
+  if (element % 2 === 0) {
+    pares.push(element);
+  } else {
+    impares.push(element);
+  }
+});
+
+console.log("Pares:", pares);
+console.log("Impares:", impares);
