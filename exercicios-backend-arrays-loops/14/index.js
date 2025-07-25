6b367be5117f047102725049dc96ab59a989431b
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-25 00:26:51 -0300

    resolvido problema 14: Imprima os menores

--- exercicios-backend-arrays-loops/14/index.js
+++ exercicios-backend-arrays-loops/14/index.js
@@ -1,3 +1,14 @@
 const arrayA = [5, 32, 3, 44, 1];
 const arrayB = [57, 4, 21, 2, 13];
 
+for (let i = 0; i < arrayB.length; i++) {
+  if (arrayB[i] < arrayA[i]) {
+    console.log(arrayB[i]);
+  } else {
+    console.log(arrayA[i]);
+  }
+}
+
+//for (let i = 0; i < arrayA.length; i++) {
+//  console.log(Math.min(arrayA[i], arrayB[i]));
+//}
