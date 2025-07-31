7d91fdbfa4b9ae2b6345f04cec71ac7f14fc2be8
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-07-31 00:44:28 -0300

    resolvido problema 5: Perda mínima

---
+++ hackerrank-arrays-loops/perdaminima.js
@@ -0,0 +1,21 @@
+const preco = [20, 15, 8, 2, 12];
+const compra = preco[1];
+
+function solucao(precos) {
+  let menorprejuizo = Infinity;
+  for (let i = 0; i < precos.length - 1; i++) {
+    for (let j = i + 1; j < precos.length; j++) {
+      const precocCompra = precos[i];
+      const precoVenda = precos[j];
+      const prejuizo = precocCompra - precoVenda;
+      if (prejuizo > 0) {
+        if (prejuizo < menorprejuizo) {
+          menorprejuizo = prejuizo;
+        }
+      }
+    }
+  }
+  return menorprejuizo;
+}
+
+console.log(solucao(preco));
