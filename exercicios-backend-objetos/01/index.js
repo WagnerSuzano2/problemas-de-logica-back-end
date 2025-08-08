4c337d9d61db39b1ed84dd47781d2d4cf35c180b
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-08-08 01:50:12 -0300

    resolvido problema 1: Cadastro de usuário

---
+++ exercicios-backend-objetos/01/index.js
@@ -0,0 +1,18 @@
+const endereco = {
+  rua: "Rua das Flores",
+  numero: 123,
+  complemento: "Apto 456",
+  bairro: "Jardim das Rosas",
+  cep: "12345-678",
+  cidade: "Cidade São Paulo",
+  estado: "Estado São Paulo",
+  pais: "País Brasil",
+};
+
+const usuario = {
+  nome: "João da Silva",
+  email: "joao.silva@example.com",
+  telefone: "11 9999-9999",
+  data_nascimento: "1995-01-01",
+  endereco: endereco,
+};
