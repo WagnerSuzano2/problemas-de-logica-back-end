850621b7cd8a25eb9913180102f624dd28c5f8dd
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-08-08 23:51:56 -0300

    resolvido problema 2: Lista de veículos

---
+++ exercicios-backend-objetos/02/index.js
@@ -0,0 +1,28 @@
+const carros = [
+  {
+    marca: "Volkswagen",
+    modelo: "Gol",
+    ano: 2022,
+    cor: "vermelho",
+    quantidade_portas: 4,
+    automatico: false,
+  },
+  {
+    marca: "Fiat",
+    modelo: "Uno",
+    ano: 2020,
+    cor: "branco",
+    quantidade_portas: 2,
+    automatico: false,
+  },
+  {
+    marca: "Honda",
+    modelo: "Civic",
+    ano: 2023,
+    cor: "preto",
+    quantidade_portas: 4,
+    automatico: true,
+  },
+];
+
+console.log(carros);
