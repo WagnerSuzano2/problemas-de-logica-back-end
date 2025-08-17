6b40a269ca423264c77f8351b7e7f611974ac90d
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-08-17 17:33:31 -0300

    resolvido problema 1: Sistema de correção de provas

--- exercicios-backend-funcoes/01/index.js
+++ exercicios-backend-funcoes/01/index.js
@@ -1,28 +1,44 @@
 const prova = {
-    aluno: "João",
-    materia: "Português",
-    valor: 10,
-    questoes: [
-        {
-            resposta: "a",
-            correta: "b"
-        },
-        {
-            resposta: "c",
-            correta: "c"
-        },
-        {
-            resposta: "e",
-            correta: "b"
-        },
-        {
-            resposta: "b",
-            correta: "b"
-        },
-        {
-            resposta: "c",
-            correta: "c"
-        }
-    ]
+  aluno: "João",
+  materia: "Português",
+  valor: 10,
+  questoes: [
+    {
+      resposta: "a",
+      correta: "b",
+    },
+    {
+      resposta: "c",
+      correta: "c",
+    },
+    {
+      resposta: "e",
+      correta: "b",
+    },
+    {
+      resposta: "b",
+      correta: "b",
+    },
+    {
+      resposta: "c",
+      correta: "c",
+    },
+  ],
 };
 
+function corrigirProva(prova) {
+  const nomeAluno = prova.aluno;
+  let acertou = 0;
+
+  for (let i = 0; i < prova.questoes.length; i++) {
+    const { resposta, correta } = prova.questoes[i];
+    if (resposta === correta) {
+      acertou++;
+    }
+  }
+  const notaFinal = (prova.valor / prova.questoes.length) * acertou;
+
+  return `O aluno(a) ${nomeAluno} acertou ${acertou} questões e obteve nota ${notaFinal}.`;
+}
+
+console.log(corrigirProva(prova));
