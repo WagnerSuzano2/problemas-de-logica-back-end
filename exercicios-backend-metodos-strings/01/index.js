e65b8c963ecbfa2a83ded60bcf63d1fc14460f6a
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-09-02 23:55:56 -0300

    feat: Conclui o exercicio 01 de metodos de strings

--- exercicios-backend-metodos-strings/01/index.js
+++ exercicios-backend-metodos-strings/01/index.js
@@ -1,1 +1,15 @@
 const comentario = "Esse COVID é muito perigoso!";
+let comentarioFinal;
+
+const comentarioEmMinusculas = comentario.toLowerCase();
+
+if (
+  comentarioEmMinusculas.includes("covid") ||
+  comentarioEmMinusculas.includes("pandemia")
+) {
+  comentarioFinal = "Comentário bloqueado por conter palavras proibidas";
+} else {
+  comentarioFinal = "Comentário autorizado";
+}
+
+console.log(comentarioFinal);
