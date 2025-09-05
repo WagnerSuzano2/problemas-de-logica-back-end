5bf2f11ebf343d4d7c1cf0625f3aee0920382c20
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-09-04 23:44:17 -0300

    feat: Conclui o exercicio 02 Sistema para formatar números

--- exercicios-backend-metodos-strings/02/index.js
+++ exercicios-backend-metodos-strings/02/index.js
@@ -1,2 +1,25 @@
 const cpf = "12345678900";
-const cnpj = "12345678900";
+const cnpj = "12345678900110";
+
+if (cpf.length === 11) {
+  let parte1 = cpf.slice(0, 3);
+  let parte2 = cpf.slice(3, 6);
+  let parte3 = cpf.slice(6, 9);
+  let parte4 = cpf.slice(9, 11);
+  let cpfFormatado = `${parte1}.${parte2}.${parte3}-${parte4}`;
+  console.log(cpfFormatado);
+} else {
+  console.log("CPF inválido");
+}
+
+if (cnpj.length === 14) {
+  let parte1 = cnpj.slice(0, 2);
+  let parte2 = cnpj.slice(2, 5);
+  let parte3 = cnpj.slice(5, 8);
+  let parte4 = cnpj.slice(8, 12);
+  let parte5 = cnpj.slice(12, 14);
+  let cnpjFormatado = `${parte1}.${parte2}.${parte3}/${parte4}-${parte5}`;
+  console.log(cnpjFormatado);
+} else {
+  console.log("CNPJ inválido");
+}
