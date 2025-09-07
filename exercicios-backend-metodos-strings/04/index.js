7d5df6e36439695858167cc4e07034819263b1da
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-09-07 20:34:19 -0300

    feat: Conclui o exercicio 04 Sistema de formatação de dados do formulário

--- exercicios-backend-metodos-strings/04/index.js
+++ exercicios-backend-metodos-strings/04/index.js
@@ -1,3 +1,18 @@
 let identificador = "123";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
+
+let identificadorZeroEsquerda = identificador.padStart(6, "0");
+let emailSemEspaco = email.trim();
+
+const nomeFormatado = nome
+  .split(" ")
+  .map((palavra) => {
+    const primeiraLetra = palavra[0].toUpperCase();
+    const restanteNome = palavra.slice(1).toLowerCase();
+    return primeiraLetra + restanteNome;
+  })
+  .join(" ");
+console.log(identificadorZeroEsquerda);
+console.log(emailSemEspaco);
+console.log(nomeFormatado);
