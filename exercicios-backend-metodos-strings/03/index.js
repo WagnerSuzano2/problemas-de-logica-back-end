28ef8264e696d6b05a37e6782b3422cd5dae2961
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-09-06 23:46:06 -0300

    feat: Conclui o exercicio 03 URL amigável

--- exercicios-backend-metodos-strings/03/index.js
+++ exercicios-backend-metodos-strings/03/index.js
@@ -1,1 +1,5 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+let tolowerCaseTexto = texto.toLowerCase();
+let arrayTexto = tolowerCaseTexto.split(" ");
+let JoinTexto = arrayTexto.join("-");
+console.log(JoinTexto);
