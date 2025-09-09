cc235c7737a9106d2fbb33f4d12b2d1e69d1124b
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2025-09-08 23:40:29 -0300

    feat: Conclui o exercicio 05 Esconder número do cartão de crédito

--- exercicios-backend-metodos-strings/05/index.js
+++ exercicios-backend-metodos-strings/05/index.js
@@ -1,1 +1,6 @@
-const numeroCartao = '1111222233334444';
+const numeroCartao = "1111222233334444";
+const ultimosQuatroDigitos = numeroCartao.slice(-4);
+const primeirosQuatroDigitos = numeroCartao.slice(0, 4);
+let asteriscos = "*".repeat(8);
+const cartaoMascarado = primeirosQuatroDigitos + asteriscos + ultimosQuatroDigitos;
+console.log(cartaoMascarado); 
